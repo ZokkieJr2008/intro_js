d5862cd361936e945f875e597156c54a12ef8caa
Ezekiel Zokoro <zokkie.jr@gmail.com>
2025-12-06 09:14:52 -0800

    BD  intro.js assignment

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Ezekiel Zokoro";
+const age = 17;
+const FavoriteCodingLanguage = "Python";
+
+console.log("Name: ", name);
+console.log("Age: ", age);
+console.log("Favorite coding language: ", FavoriteCodingLanguage);
